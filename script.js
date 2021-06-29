abe7854ab6bd61383b81a7e1ef74fe4f5bc56833
Katarzyna Chelminska <chelminska.katarzyna@gmail.com>
2021-06-29 16:41:46 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let activeElement = 0;
+const timeChange = 4000;
+ 
+const colorImgHtml = document.querySelector(".color");
+const grayImgHtml = document.querySelector(".gray");
+const h1Html = document.querySelector(".member h1");
+const h2Html = document.querySelector(".member h2");
+
+const colorImages = ["img/s1.png", "img/s2.png", "img/s3.png"];
+const grayImages = ["img/s1a.png", "img/s2a.png", "img/s3a.png"];
+const names = ["Anna Kowalska", "Tomasz Nowak", "Bartosz Wiśniewski"];
+const professions = ["Programistka JS", "FrontEnd Developer", "UX Designer"];
+
+function changeElement(){
+    activeElement++;
+    if (activeElement == colorImages.length) {
+        activeElement = 0;
+    }
+    colorImgHtml.src = colorImages[activeElement];
+    grayImgHtml.src = grayImages[activeElement];
+    h1Html.textContent = names[activeElement];
+    h2Html.textContent = professions[activeElement];
+}
+
+setInterval(changeElement, timeChange)
